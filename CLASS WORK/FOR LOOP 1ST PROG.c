1c351fba86eb6534dbe51a662d152fa16ed773c0
Harshuv9 <harshitpatelv9@gmail.com>
2022-12-08 21:00:28 -0800

    Create FOR LOOP 1ST PROG.c

---
+++ CLASS WORK/FOR LOOP 1ST PROG.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/*
+	for loop : All in one line
+	
+	syntax: 
+				for(intialization;condition;updation)
+				{
+					statement;
+				}
+	
+*/
+
+void main()
+{
+	int i;
+	
+	for(i=1;i<=10;i++)
+	{
+		printf("%d\n",i);		
+	}
+}
+
+
+
+
+
+
+
+
