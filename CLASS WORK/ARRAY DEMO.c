c0e4618c7b995b171323ceb5fb47be257db99329
Harshuv9 <harshitpatelv9@gmail.com>
2022-12-11 08:55:17 -0800

    Create ARRAY DEMO.c

---
+++ CLASS WORK/ARRAY DEMO.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+	int a[50] = {10,20,30,40,50};
+	
+	printf("%d\t",a[0]);
+	printf("%d\t",a[1]);
+	printf("%d\t",a[2]);
+	printf("%d\t",a[3]);
+	printf("%d\t",a[4]);
+}
