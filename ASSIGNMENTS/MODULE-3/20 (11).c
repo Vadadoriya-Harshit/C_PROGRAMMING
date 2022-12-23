6f72b2fe47df2bfa15b17da2673261b03692d386
Harshuv9 <harshitpatelv9@gmail.com>
2022-12-22 22:33:17 -0800

    Create 20 (11).c

---
+++ ASSIGNMENTS/MODULE-3/20 (11).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+	int i,j,k;
+	for(i=1;i<=5;i++)
+	{
+		for(k=0;k<=5-i;k++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf(" %d",j%2!=0);
+		}
+		printf("\n");
+	}
+}
