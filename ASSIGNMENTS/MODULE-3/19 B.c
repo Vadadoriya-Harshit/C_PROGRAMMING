d7265e1ada3a3cbc1a00ce5786bc29b406d4603b
Harshuv9 <harshitpatelv9@gmail.com>
2022-12-22 22:32:31 -0800

    Create 19 B.c

---
+++ ASSIGNMENTS/MODULE-3/19 B.c
@@ -0,0 +1,15 @@
+//b. Write a Program to print the 51 to60 using while loop//
+#include<stdio.h>
+
+void main()
+{
+	int i=51;
+	printf("/n/t\xb2\xb2\xb2\xb2 while loop \xb2\xb2\xb2\xb2");
+    while(i<=60)
+    {
+    	printf("\n\t%d",i);
+    	i++;
+	}
+    
+}
+
