4e45e038156ed378f497e34b3b7572a2a14be1f2
Harshuv9 <harshitpatelv9@gmail.com>
2022-12-22 22:34:10 -0800

    Create 3.c

---
+++ ASSIGNMENTS/MODULE-3/3.c
@@ -0,0 +1,19 @@
+ //3. Write a program to make a square and cube of number.//
+
+#include<stdio.h>
+
+void main()
+{
+ int a,s,c;
+ 
+printf("\n\n\t\t\xb2\xb2\xb2\xb2 SQUARE & CUBE \xb2\xb2\xb2\xb2\n");
+ printf("\n\t\t Enter Number A: ");
+ scanf("%d",&a);
+
+ s=a*a;  
+ c=s*a;  
+
+ printf("\n\n\t\t Square of %d is = %d",a,s);
+ printf("\n\n \t\tCube of %d is   = %d",a,c);
+
+}
