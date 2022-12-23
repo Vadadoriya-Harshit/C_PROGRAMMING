8c266dcc00fc2ddb11a7c4d128a2615d95abca2a
Harshuv9 <harshitpatelv9@gmail.com>
2022-12-22 22:32:48 -0800

    Create 19 H.c

---
+++ ASSIGNMENTS/MODULE-3/19 H.c
@@ -0,0 +1,14 @@
+
+
+
+
+//	Operators in C.
+//
+//	1) Arithmetic Operator 		: +-*/
+//	2) Logical Operator    		: && (AND),||(OR),!(NOT)
+//	3) Comparison Operator		: == ,=>,=<,>=,<=,!=
+//	4) Assignment Operator		: =, =+,-=,*=,/=
+//	5) Ternary Operator		  ?:
+//	6) Unary Operator		: ++ (increment),--(decrement)
+//	
+//	
