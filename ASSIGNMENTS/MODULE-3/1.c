70c49fd946f925cd7a32019ca3598675e0fd7b03
Harshuv9 <harshitpatelv9@gmail.com>
2022-12-22 22:31:55 -0800

    Create 1.c

---
+++ ASSIGNMENTS/MODULE-3/1.c
@@ -0,0 +1,15 @@
+/*1.Display This Information using printf
+       a.Your Name
+       b.Your Birth date
+       c.Your Age
+       d.Your Address*/
+#include<stdio.h>
+
+void main()
+{
+	printf("\n\n\t\t\xb2\xb2\xb2\xb2 PERSNAL DETAILS \xb2\xb2\xb2\xb2\n");
+	printf("\n\n\tNAME       : VADADORIYA HARSHIT AMITBHAI");
+	printf("\n\n\tBIRTH DATE : 06/08/2005");
+	printf("\n\n\tAGE        : 17 YEARS");
+	printf("\n\n\tADDRESS    : BALEL PIPARIYA,AMRELI,GUJARTA,365460");
+}
