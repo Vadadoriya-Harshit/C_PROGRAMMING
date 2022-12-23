13e0a30faf120e58c8f23b38554d6337fcf36133
Harshuv9 <harshitpatelv9@gmail.com>
2022-12-22 22:32:28 -0800

    Create 19 A.c

---
+++ ASSIGNMENTS/MODULE-3/19 A.c
@@ -0,0 +1,15 @@
+ //  a. Write aprogramto printthe 1to 10usingfor loop.
+
+#include<stdio.h>
+
+void main()
+{
+	int i;
+	printf("/n/t\xb2\xb2\xb2\xb2 for loop \xb2\xb2\xb2\xb2");
+     
+     for(i=1;i<=10;i++)
+     {
+     	printf("\n\t%d",i);
+	 }
+     
+}
