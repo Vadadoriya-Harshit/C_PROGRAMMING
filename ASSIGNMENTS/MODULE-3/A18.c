93f17eea75eb4cd7f6d0c3eb2a300b002c9df7a1
Harshuv9 <harshitpatelv9@gmail.com>
2022-12-22 22:35:24 -0800

    Create A18.c

---
+++ ASSIGNMENTS/MODULE-3/A18.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void f1()
+{
+	int a=3,s,c;
+	s=a*a;
+	c=s*a;
+	
+	printf("\n\t\tTHE SQUARE OF A IS : %d",s);
+	printf("\n\n\t\tTHE CUBE OF A IS : %d",c);
+	
+}
+
+void main()
+{
+	f1();
+}
+	
