ee4cf72d92cdfca4a7225b912566a98a546078f5
Harshuv9 <harshitpatelv9@gmail.com>
2022-12-22 22:31:58 -0800

    Create 10.c

---
+++ ASSIGNMENTS/MODULE-3/10.c
@@ -0,0 +1,25 @@
+   /*10. Write a Program to check the given number
+       is Positive, Negative.*/
+
+#include<stdio.h>
+
+void main()
+{
+	printf("\n\t\xb2\xb2\xb2\xb2\xb2\xb2\xb2\xb2\xb2 CHECK THE GIVEN NUMBER IS POSITIVE OR NAGATIVE\xb2\xb2\xb2\xb2\xb2\xb2\xb2\xb2\xb2\n");
+	int a;
+	
+	printf("\n\t\t\t\t Enter A :");
+	scanf("%d",&a);
+	printf("\n\t\t\t\t A Is : %d",a);
+	
+	if(a>0)
+	{
+		printf("\n\t\t\t\t%d is positive number.",a);
+	}
+	else
+	{
+		printf("\n\n\t\t\t\t%d is nagative number.",a);
+	}
+}
+
+
