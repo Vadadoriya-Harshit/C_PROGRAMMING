8dd2c5b319ca3c649cbb98ecce223e7c9cdbd579
Harshuv9 <harshitpatelv9@gmail.com>
2022-12-22 22:32:34 -0800

    Create 19 C.c

---
+++ ASSIGNMENTS/MODULE-3/19 C.c
@@ -0,0 +1,14 @@
+ //c. write aprogramto printthe 100to 81using do....while loop
+#include<stdio.h>
+
+void main()
+{
+	int i=100;
+	printf("/n/t\xb2\xb2\xb2\xb2 do while loop \xb2\xb2\xb2\xb2");
+	do
+	{
+		printf("\n\t%d",i);
+		i--;
+	}
+	while(i>80);
+}
