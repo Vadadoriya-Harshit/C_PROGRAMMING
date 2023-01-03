1ff0622cf0cbb520a0f49647cd91bfd9bdad84d0
Harshuv9 <harshitpatelv9@gmail.com>
2023-01-02 23:20:43 -0800

    Create POINTER DEMO 2.cpp

---
+++ CLASS WORK/POINTER DEMO 2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int x=12;
+	int y=32;
+	int *x_ptr;
+	int *y_ptr;
+	x_ptr=&x;
+	y_pr=&y;
+	
+	
+	printf("\n\n\t\t x=%d",X_ptr);
+	printf("\n\n\t\t x=%d",y_ptr);
+	
+	
+}
