adaa3d50fa4417eff3a8904a8d4e1eb711a04da7
Harshuv9 <harshitpatelv9@gmail.com>
2023-01-02 23:21:06 -0800

    Create POINTER TO ARRAY OF STRING.c

---
+++ CLASS WORK/POINTER TO ARRAY OF STRING.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+	char *ipl[]={"gujarat","maharashtr","rajasthan","tamilnadu","keral","aasam","bihar","jammu&kashmir"};
+	int i;
+	printf("state of india\n\n");
+	for(i=0;i<8;i++)
+	printf(">> %s\n",ipl[i]);
+	
+	
+}
