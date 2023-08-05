c91b309c77925d38a38d461400241dbe6fc1b3db
Harshuv9 <harshitpatelv9@gmail.com>
2023-08-05 17:23:17 +0530

    .

---
+++ CLASS WORK/harsh.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+      char name[12]="Harsh";
+      int a=22;
+      
+     printf("Value of a is : %d \n\n",a);
+    printf("My Name is : %s",name);
+      
+}
+
