28f61cdc7055fbfcb3712015bbaeb212e0397bbe
Harshuv9 <harshitpatelv9@gmail.com>
2023-09-21 10:08:51 +0530

    .

---
+++ LAB WORK/cc.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+
+int main()
+{
+	int a="hello world";
+      printf(sizeof(a));
+	return 0;
+}
---
+++ LAB WORK/state.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string name;
+	cin<<"Enter Name : ",&name;
+	cout>>name;
+	return 0;
+}
+
+
+
