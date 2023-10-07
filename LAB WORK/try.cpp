1367c5f4662f839922ff7bdf7122e99cb20d1fc1
Vadadoriya-Harshit <harshitpatelv9@gmail.com>
2023-10-07 14:44:25 +0530

    .

--- LAB WORK/try.cpp
+++ LAB WORK/try.cpp
@@ -1,18 +1,1 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int i,j;
-	
-	for(i=0;i<=5;i++)
-	{
-		for(j=0;j<=i;j++)
-		{
-			cout<<" *";
-		}
-		cout<<"\n";
-	}
-	
-	
-	return 0;
-}
+uj
